2816ea459187f37629a8b6de4954939839b9602c
NeilKleistGao <neilkleist@gmail.com>
2023-08-15 16:58:54 +0800

    Init project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lich "0.1.0"
+  :description "LIcensed touCH"
+  :url "https://github.com/NeilKleistGao/lich"
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html#license-text"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main lich.main)
