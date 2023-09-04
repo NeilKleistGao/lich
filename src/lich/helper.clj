0695d2ebd8c4e6712a113bd19fedfb529bc5486d
NeilKleistGao <neilkleist@gmail.com>
2023-09-04 15:49:34 +0800

    Find license

--- src/lich/helper.clj
+++ src/lich/helper.clj
@@ -1,3 +1,22 @@
-(ns lich.helper)
+(ns lich.helper
+  (:require clojure.java.io
+            [clojure.string :as strs])
+  (:require clojure.pprint)
+  (:require [clojure.java.shell :as shell]))
 
+(defmacro does-not-exist? [filename]
+  `(not (.exists (clojure.java.io/file ~filename))))
 
+(defmacro ^:private sh [cmd & args]
+  `(shell/sh ~cmd ~@args))
+
+(defn- get-git-root [] ; TODO: check the directory
+  (try
+    (sh "git" "rev-parse" "--show-toplevel")
+    (catch Exception e (.getMessage e)))) ; use current dir as the git root
+
+(defn get-license []
+  (let [path (get (get-git-root) :out ".") file (str (strs/replace path #"\n" "") "/LICENSE")]
+    (if (does-not-exist? file)
+      ""
+      (slurp file)))) ; TODO: get the file
--- src/lich/touch.clj
+++ src/lich/touch.clj
@@ -1,18 +1,18 @@
 (ns lich.touch
   (:require clojure.java.io)
-  (:require [clojure.string :as strs]))
+  (:require [clojure.string :as strs])
+  (:require lich.helper))
 
-(defn- does-not-exist? [filename]
-  (not (.exists (clojure.java.io/file filename))))
+(def ^:private max-lines 20)
 
-(defmacro format-comment [start-line end-line mid-start mid-end comment]
+(defmacro ^:private format-comment [start-line end-line mid-start mid-end get-comment]
   `(str ~start-line
         "\n"
-        (strs/join "\n" (map #(str ~mid-start % ~mid-end) (strs/split-lines ~comment)))
+        (strs/join "\n" (map #(str ~mid-start % ~mid-end) (take max-lines (strs/split-lines (~get-comment))))) ; TODO: config
         "\n"
         ~end-line))
 
 (defn touch-with-license [filename]
-  (if (does-not-exist? filename)
-    (spit filename (format-comment "/**" " */" " * " "" "foo\nbar\nbaz")) ;; TODO: config
+  (if (lich.helper/does-not-exist? filename)
+    (spit filename (format-comment "/**" " */" " * " "" lich.helper/get-license)) ;; TODO: config
     ())) ;; TODO: support existed files
